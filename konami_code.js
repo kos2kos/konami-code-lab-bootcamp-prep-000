e3b9aa4eaeb18df0970c84e571f78e7bc446f027
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-04-15 01:54:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -22,7 +22,7 @@ function init() {
   if (key === code[index]) {
     index++;
     if (index === code.length) {
-      alert("YOOOOOOKOOOONAMI")
+      alert("YOOOOOOKOOOONAMI");
       konamiCodePosition = 0;
     }
   } else {
